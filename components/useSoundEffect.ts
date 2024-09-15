4e44137cf304e1f86ced5d2c7f4524ab54aa0b08
hayzedd2 <azeezalhameen1@gmail.com>
2024-09-15 23:18:20 +0100

    ios optimization workaround

--- components/useSoundEffect.ts
+++ components/useSoundEffect.ts
@@ -1,20 +1,54 @@
-import { useRef, useCallback } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 
 export const useSound = (soundPath: string) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const isLoadedRef = useRef(false);
+
+  useEffect(() => {
+    const audio = new Audio(soundPath);
+    audioRef.current = audio;
+    const handleCanPlayThrough = () => {
+      isLoadedRef.current = true;
+    };
+    audio.addEventListener('canplaythrough', handleCanPlayThrough);
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      audio.load();
+    }
+
+    return () => {
+      audio.removeEventListener('canplaythrough', handleCanPlayThrough);
+      audio.pause();
+      audioRef.current = null;
+    };
+  }, [soundPath]);
 
   const playSound = useCallback(() => {
-    if (!audioRef.current) {
-      audioRef.current = new Audio(soundPath);
+    const audio = audioRef.current;
+    if (audio) {
+      if (!isLoadedRef.current) {
+        audio.load();
+      }
+      
+      // iOS workaround
+      if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        audio.muted = true;
+        audio.play().then(() => {
+          audio.muted = false;
+          audio.currentTime = 0;
+          audio.play().catch(error => {
+            console.error('Error playing sound:', error);
+          });
+        }).catch(error => {
+          console.error('Error during iOS audio workaround:', error);
+        });
+      } else {
+        audio.currentTime = 0;
+        audio.play().catch(error => {
+          console.error('Error playing sound:', error);
+        });
+      }
     }
-
-    if (audioRef.current) {
-      audioRef.current.currentTime = 0; // Reset to start
-      audioRef.current.play().catch(error => {
-        console.error('Error playing sound:', error);
-      });
-    }
-  }, [soundPath]);
+  }, []);
 
   return playSound;
 };
